d2bf0cc4e80ea44b8d6ae1b5d6f14d8328d23f72
Yiğit Can Ayaz <54958108+YigitCanAyaz@users.noreply.github.com>
2023-04-19 08:16:13 +0300

    Add project files.

---
+++ RabbitMQPractice.Publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+
+// In the real scenario just put this url in appsettings.json
+factory.Uri = new Uri("amqps://wxrhdmxb:Z4BKct0LOg1frdiJYsxjdhxp05K2vHMk@woodpecker.rmq.cloudamqp.com/wxrhdmxb");
+
+// end of the main scope it will be discarded
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+// creatin queue
+channel.QueueDeclare("hello-queue", true, false, false);
+
+string message = "hello world";
+
+// converting to byte because rabbitmq only takes byte as an input
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+// string.Empty bcs we r not using exchange yet
+// hello-queue => routingKey
+channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+
+Console.WriteLine("Mesaj gönderilmiştir");
+
+Console.ReadLine();
---
+++ RabbitMQPractice.Subscriber/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+
+factory.Uri = new Uri("amqps://wxrhdmxb:Z4BKct0LOg1frdiJYsxjdhxp05K2vHMk@woodpecker.rmq.cloudamqp.com/wxrhdmxb");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+// optional to use, if we sure that this queue exist in the publisher side we don't need this code, this code will generate queue if doesn't exist. we know that this queue exist in the publisher side we don't this code really. (parameters should be same too!)
+// channel.QueueDeclare("hello-queue", true, false, false);
+
+// consumer means subscriber
+var consumer = new EventingBasicConsumer(channel);
+
+// autoAck => false (just send success messages)
+channel.BasicConsume("hello-queue", true, consumer);
+
+consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    Console.WriteLine("The message: " + message);
+};
+
+Console.ReadLine();
